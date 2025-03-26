92e0c665a3e02371c55a0956ae7e5b213e65c581
WISP <144270275+MatveyTolstov@users.noreply.github.com>
2025-03-26 23:10:23 +0300

    Проверка pull request

--- src/main/java/com/example/mvd_balaha/MvdBalahaApplication.java
+++ src/main/java/com/example/mvd_balaha/MvdBalahaApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MvdBalahaApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MvdBalahaApplication.class, args);
+
+        System.out.println("MvdBalahaApplication started");
     }
 
 }
