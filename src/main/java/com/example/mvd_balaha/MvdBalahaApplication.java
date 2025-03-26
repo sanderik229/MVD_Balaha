64bb339734f77e1da5ac9a443d49381756744e0a
sanderik229 <144127176+sanderik229@users.noreply.github.com>
2025-03-26 23:06:25 +0300

    Создал проект

---
+++ src/main/java/com/example/mvd_balaha/MvdBalahaApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvd_balaha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvdBalahaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvdBalahaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mvd_balaha/MvdBalahaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvd_balaha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvdBalahaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
