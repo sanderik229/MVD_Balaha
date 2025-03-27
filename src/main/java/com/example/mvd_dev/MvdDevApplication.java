433b816934ebad6e06bca8fa2c685d8f5f4dce5f
WISP <144270275+MatveyTolstov@users.noreply.github.com>
2025-03-27 13:25:25 +0300

    Пересоздание проекта
    -Был выбран новый норм установщик xml вместо gradle
    -Добавлен новый сервис для авторизации Spring Security
    -Добавлен драйвер для PostgreSql
    -Добавлен ORM JPA
    -Добавлен Spring Java MVC
    -Добавлен Lombok для упрощения кода

---
+++ src/main/java/com/example/mvd_dev/MvdDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvd_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvdDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvdDevApplication.class, args);
+    }
+
+}
